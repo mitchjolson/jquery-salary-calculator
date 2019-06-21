b90d8d1a443ba7a599eeb573b243542dc0f3f984
Mitch Olson <mitchjolson@gmail.com>
2019-06-21 16:15:45 -0500

    add employee functionality added

--- scripts/client.js
+++ scripts/client.js
@@ -1,5 +1,29 @@
 $(document).ready(onReady);
 
-function onReady(){
+let employees = [];
 
+function onReady(){
+    $('#submitButton').on('click', submit);
+}
+
+function submit(){
+    console.log('clicked submit');
+    let newEmployee = {
+        firstName: $('#firstNameIn').val(),
+        lastName: $('#lastNameIn').val(),
+        idNumber: $('#idNumberIn').val(),
+        jobTitle: $('#jobTitleIn').val(),
+        salary: $('#salaryIn').val(),
+    }
+    console.log( 'adding: ', newEmployee );
+    employees.push( newEmployee );
+    showEmployee();
+}
+
+function showEmployee(){
+    let el = $( '#employeesOut' );
+    el.empty();
+    for( let i=0; i < employees.length; i++ ){
+        el.append( `<li>${employees[i]}</li>` );
+    }
 }

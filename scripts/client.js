07f8af3a7307b629ad9c21fe74d51e5419aeb283
Mitch Olson <mitchjolson@gmail.com>
2019-06-21 15:23:05 -0500

    added header with css

---
+++ scripts/client.js
@@ -0,0 +1,8 @@
+// The application should have an input form that collects employee 
+// first name, last name, ID number, job title, annual salary.
+
+$(document).ready(onReady);
+
+function onReady(){
+    
+}

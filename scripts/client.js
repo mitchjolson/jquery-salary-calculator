036b34c0c8ac4ee69c44e7dc50df5271e0aae862
Mitch Olson <mitchjolson@gmail.com>
2019-06-21 15:37:17 -0500

    added input fields

--- scripts/client.js
+++ scripts/client.js
@@ -1,8 +1,5 @@
-// The application should have an input form that collects employee 
-// first name, last name, ID number, job title, annual salary.
-
 $(document).ready(onReady);
 
 function onReady(){
-    
+
 }
